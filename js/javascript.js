7a4e020c3919f701301cc6be75e8e37254758ec1
Rodrigo Ribeiro <digoribeiro@MacBook-Pro-de-Rodrigo.local>
2020-03-14 16:20:15 -0300

    validacação com texto

--- js/javascript.js
+++ js/javascript.js
@@ -3,10 +3,42 @@ $('.ver-mais').on('click', function() {
   $('.ver-mais').hide();
 });
 
-$('input, textarea').blur(function() {
-  if ($(this).val() === '') {
+$('.nome').blur(function() {
+  if ($('.nome').val() === '') {
     $(this).addClass('erro');
+    $('.validacao.nome').show();
   } else {
     $(this).removeClass('erro');
+    $('.validacao.nome').hide();
+  }
+});
+
+$('.email').blur(function() {
+  if ($('.email').val() === '') {
+    $(this).addClass('erro');
+    $('.validacao.email').show();
+  } else {
+    $(this).removeClass('erro');
+    $('.validacao.email').hide();
+  }
+});
+
+$('.telefone').blur(function() {
+  if ($('.telefone').val() === '') {
+    $(this).addClass('erro');
+    $('.validacao.telefone').show();
+  } else {
+    $(this).removeClass('erro');
+    $('.validacao.telefone').hide();
+  }
+});
+
+$('.mensagem').blur(function() {
+  if ($('.mensagem').val() === '') {
+    $(this).addClass('erro');
+    $('.validacao.mensagem').show();
+  } else {
+    $(this).removeClass('erro');
+    $('.validacao.mensagem').hide();
   }
 });
