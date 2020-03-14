2d452a99a237cc1bbc1734c4e0186cdb267bf08f
Rodrigo Ribeiro <digoribeiro@MacBook-Pro-de-Rodrigo.local>
2020-03-14 15:42:47 -0300

    add validação de input

--- js/javascript.js
+++ js/javascript.js
@@ -2,3 +2,11 @@ $('.ver-mais').on('click', function() {
   $('.resto-curriculo').show();
   $('.ver-mais').hide();
 });
+
+$('input, textarea').blur(function() {
+  if ($(this).val() === '') {
+    $(this).addClass('erro');
+  } else {
+    $(this).removeClass('erro');
+  }
+});
